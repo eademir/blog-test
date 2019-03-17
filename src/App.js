cd45c3c5523c36bdd86e2656730ffd277b7db446
eademir <eraydemir1@yandex.com>
2019-03-17 16:20:52 +0300

    .

--- src/App.js
+++ src/App.js
@@ -3,62 +3,21 @@ import { Link, Route } from 'react-router-dom'
 import BlogPage from './views/Blog/blog'
 import 'semantic-ui-css/semantic.min.css'
 import {Container, Divider, Dropdown, Grid, Header, Icon, Image, List, Menu, Segment, Visibility,} from 'semantic-ui-react'
-
-const menuStyle = {
-    border: 'none',
-    borderRadius: 0,
-    boxShadow: 'none',
-    marginBottom: '1em',
-    transition: 'box-shadow 0.5s ease, padding 0.5s ease',
-}
-
-const fixedMenuStyle = {
-    backgroundColor: '#fff',
-    border: '1px solid #ddd',
-    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
-}
-
-const overlayStyle = {
-    float: 'left',
-    margin: '0em 3em 1em 0em',
-}
-
-const fixedOverlayStyle = {
-    ...overlayStyle,
-    position: 'fixed',
-    top: '80px',
-    zIndex: 10,
-}
-
-const overlayMenuStyle = {
-    position: 'relative',
-    left: 0,
-    transition: 'left 0.5s ease',
-}
-
-const fixedOverlayMenuStyle = {
-    ...overlayMenuStyle,
-    left: '800px',
-}
+import { menuStyle, fixedMenuStyle} from "./helper/stylehelper";
 
 class App extends Component {
 
     state = {
         menuFixed: false,
         overlayFixed: false,
-    }
+    };
 
+    stickTopMenu = () => this.setState({ menuFixed: true });
 
-    stickOverlay = () => this.setState({ overlayFixed: true })
-
-    stickTopMenu = () => this.setState({ menuFixed: true })
-
-    unStickOverlay = () => this.setState({ overlayFixed: false })
-
-    unStickTopMenu = () => this.setState({ menuFixed: false })
+    unStickTopMenu = () => this.setState({ menuFixed: false });
 
   render() {
-      const { menuFixed, overlayFixed, overlayRect } = this.state
+      const { menuFixed, overlayFixed, overlayRect } = this.state;
     return (
       <div>
 
@@ -85,61 +44,12 @@ class App extends Component {
                       </Menu.Item>
                       <Menu.Item as='a'>Add Blog</Menu.Item>
 
-                      <Menu.Menu position='right'>
-                          <Dropdown text='Dropdown' pointing className='link item'>
-                              <Dropdown.Menu>
-                                  <Dropdown.Item>List Item</Dropdown.Item>
-                                  <Dropdown.Item>List Item</Dropdown.Item>
-                                  <Dropdown.Divider />
-                                  <Dropdown.Header>Header Item</Dropdown.Header>
-                                  <Dropdown.Item>
-                                      <i className='dropdown icon' />
-                                      <span className='text'>Submenu</span>
-                                      <Dropdown.Menu>
-                                          <Dropdown.Item>List Item</Dropdown.Item>
-                                          <Dropdown.Item>List Item</Dropdown.Item>
-                                      </Dropdown.Menu>
-                                  </Dropdown.Item>
-                                  <Dropdown.Item>List Item</Dropdown.Item>
-                              </Dropdown.Menu>
-                          </Dropdown>
-                      </Menu.Menu>
                   </Container>
               </Menu>
           </Visibility>
 
           <Container text>
               <Route path={'/blogs'} component={BlogPage}/>
-              <Visibility
-                  offset={80}
-                  once={false}
-                  onTopPassed={this.stickOverlay}
-                  onTopVisible={this.unStickOverlay}
-                  style={overlayFixed ? { ...overlayStyle, ...overlayRect } : {}}
-              />
-
-              <div ref={this.handleOverlayRef} style={overlayFixed ? fixedOverlayStyle : overlayStyle}>
-                  <Menu
-                      icon='labeled'
-                      style={overlayFixed ? fixedOverlayMenuStyle : overlayMenuStyle}
-                      vertical
-                  >
-                      <Menu.Item>
-                          <Icon name='twitter' />
-                          Twitter
-                      </Menu.Item>
-
-                      <Menu.Item>
-                          <Icon name='facebook' />
-                          Share
-                      </Menu.Item>
-
-                      <Menu.Item>
-                          <Icon name='mail' />
-                          Email
-                      </Menu.Item>
-                  </Menu>
-              </div>
 
           </Container>
 
